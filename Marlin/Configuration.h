d219e41d059f68c3a257afb2067a256399361bb3
Rob Cowie <asbestospiping@gmail.com>
2016-02-10 08:02:49 +0000

    Tweak z-offset to fix adhesion issue

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -411,7 +411,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
   // X and Y offsets must be integers
   #define X_PROBE_OFFSET_FROM_EXTRUDER 0
   #define Y_PROBE_OFFSET_FROM_EXTRUDER 0
-  #define Z_PROBE_OFFSET_FROM_EXTRUDER .9
+  #define Z_PROBE_OFFSET_FROM_EXTRUDER .8
 
   #define Z_RAISE_BEFORE_HOMING 5       // (in mm) Raise Z before homing (G28) for Probe Clearance.
                                         // Be sure you have this distance over your Z_MAX_POS in case
